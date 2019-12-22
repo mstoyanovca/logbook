f457434964c8e92871b8c1dd22b17a030fa9bc8a
mstoyanovca <mstoyanovca@gmail.com>
2019-12-21 22:09:31 -0500

    refactored add qso form

--- app/dao/QsoDao.scala
+++ app/dao/QsoDao.scala
@@ -9,13 +9,13 @@ import model.Qso
 class QsoDao @Inject()() {
   def findAll(): Seq[Qso] = {
     var qsos = Seq(
-      Qso(Some(1L), Some(1L), LocalDate.of(2019, 12, 15), LocalTime.of(22, 0), "LZ2AB", "144.000", "FM", "Jivko", Some("Balchik"), Some(100), Some("59+"), Some("58"), Some("HT")),
-      Qso(Some(2L), Some(1L), LocalDate.of(2019, 12, 16), LocalTime.of(22, 52), "LZ1CD", "14.085", "CW", "Ivan", Some("Karlovo"), Some(20), Some("588"), Some("46"), Some("Vetical")),
-      Qso(Some(3L), Some(1L), LocalDate.of(2018, 12, 1), LocalTime.of(21, 40), "LZ2EF", "3.564", "SSB", "Miro", Some("Sofia"), Some(50), Some("466"), Some("59"), Some("Ground Plane")),
-      Qso(Some(1L), Some(1L), LocalDate.of(2016, 12, 15), LocalTime.of(2, 51), "LZ4MN", "28.800", "FT8", "Kiro", Some("Turnovo"), Some(200), Some("598"), Some("59"), Some("JPole"))
+      Qso(None, None, LocalDate.of(2019, 12, 15), LocalTime.of(22, 0), "LZ2AB", "144.000", "FM", "59+", Some("58"), Some(100), Some("Jivko"), Some("Balchik"), Some("HT")),
+      Qso(None, None, LocalDate.of(2019, 12, 16), LocalTime.of(22, 52), "LZ1CD", "14.085", "CW", "588", Some("46"), Some(20), Some("Ivan"), Some("Karlovo"), Some("Vetical")),
+      Qso(None, None, LocalDate.of(2018, 12, 1), LocalTime.of(21, 40), "LZ2EF", "3.564", "SSB", "466", Some("59"), Some(50), Some("Miro"), Some("Sofia"), Some("Ground Plane")),
+      Qso(Some(1L), Some(1L), LocalDate.of(2016, 12, 15), LocalTime.of(2, 51), "LZ4MN", "28.800", "FT8", "598", Some("59"), Some(200), Some("Kiro"), Some("Turnovo"), Some("JPole"))
     )
     for (i <- 0 to 20) {
-      qsos = qsos :+ Qso(Some(5 + i), Some(1L), LocalDate.of(2018, 12, i + 1), LocalTime.of(21, 20 + i), "LZ2KVV", "7.150", "SSB", "Ivan", Some("Varna"), Some(100), Some("59"), Some("48"), Some("Dipole"))
+      qsos = qsos :+ Qso(None, None, LocalDate.of(2018, 12, i + 1), LocalTime.of(21, 20 + i), "LZ2KVV", "7.150", "SSB", "59", Some("48"), Some(100), Some("Ivan"), Some("Varna"), Some("Dipole"))
     }
     qsos
   }
--- app/model/Qso.scala
+++ app/model/Qso.scala
@@ -4,19 +4,19 @@ import java.time.{LocalDate, LocalTime}
 
 import play.api.libs.json.{Json, Reads, Writes}
 
-case class Qso(id: Option[Long],
-               userId: Option[Long],
+case class Qso(id: Option[Long] = None,
+               userId: Option[Long] = None,
                date: LocalDate,
                time: LocalTime,
                callsign: String,
                frequency: String,
                mode: String,
-               name: String,
-               qth: Option[String],
-               power: Option[Int],
-               rstSent: Option[String],
-               rstReceived: Option[String],
-               notes: Option[String])
+               rstSent: String,
+               rstReceived: Option[String] = None,
+               power: Option[Int] = None,
+               name: Option[String] = None,
+               qth: Option[String] = None,
+               notes: Option[String] = None)
 
 object Qso {
   implicit val reads: Reads[Qso] = Json.reads[Qso]
