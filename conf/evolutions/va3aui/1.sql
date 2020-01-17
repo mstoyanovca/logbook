554f5bf66158ef7917f8b1d4594936086d35b581
mstoyanovca <mstoyanovca@gmail.com>
2020-01-16 21:57:24 -0500

    fixed the sql

--- conf/evolutions/va3aui/1.sql
+++ conf/evolutions/va3aui/1.sql
@@ -7,8 +7,6 @@ create table user (
   primary key (id)
 );
 
-insert into user values (1, "mstoyanovca@gmail.com", "password");
-
 create table qso(
 id bigint not null auto_increment,
 user_id bigint not null,
@@ -18,7 +16,7 @@ frequency varchar(16) not null,
 mode varchar(16) not null,
 rst_sent varchar(16) not null,
 rst_received varchar(16),
-power int,
+power varchar(16),
 name varchar(32),
 qth varchar(32),
 notes varchar(255),
@@ -26,11 +24,13 @@ primary key (id),
 foreign key (user_id) references user(id)
 );
 
-insert into qso values(1, 1, '2019-12-15 22:00:00', "LZ2AB", "144.000", "FM", "59+", "58", 100, "Jivko", "Balchik", "HT");
-insert into qso values(2, 1, '2019-12-16 22:00:00', "LZ1CD", "14.085", "CW", "588", "46", 20, "Ivan", "Karlovo", "Vetical");
-insert into qso values(3, 1, '2018-12-1 21:40:00', "LZ2EF", "3.564", "SSB", "466", "59", 50, "Miro", "Sofia", "Ground Plane");
-insert into qso values(4, 1, '2016-12-15 2:45:00', "LZ4MN", "28.800", "FT8", "598", "59", 200, "Kiro", "Turnovo", "JPole");
-insert into qso values(5, 1, '2016-12-15 2:58:00', "LZ4MN", "28.815", "FT8", "598", "59", 200, "Kiro", "Turnovo", "JPole");
+insert into user values (0, "mstoyanovca@gmail.com", "password");
+
+insert into qso values(0, (select id from user where email="mstoyanovca@gmail.com"), "2019-12-15 22:00:00", "LZ2AB", "144.000MHz", "FM", "59+", "58", "100W", "Jivko", "Balchik", "HT");
+insert into qso values(0, (select id from user where email="mstoyanovca@gmail.com"), "2019-12-16 22:00:00", "LZ1CD", "14.085MHz", "CW", "588", "46", "20W", "Ivan", "Karlovo", "Vetical");
+insert into qso values(0, (select id from user where email="mstoyanovca@gmail.com"), "2018-12-1 21:40:00", "LZ2EF", "3.564MHz", "SSB", "466", "59", "50W", "Miro", "Sofia", "Ground Plane");
+insert into qso values(0, (select id from user where email="mstoyanovca@gmail.com"), "2016-12-15 2:45:00", "LZ4MN", "28.800MHz", "FT8", "598", "59", "200W", "Kiro", "Turnovo", "JPole");
+insert into qso values(0, (select id from user where email="mstoyanovca@gmail.com"), "2016-12-15 2:58:00", "LZ4MN", "28.815MHz", "FT8", "598", "59", "200W", "Kiro", "Turnovo", "JPole");
 
 -- !Downs
 drop table user;
