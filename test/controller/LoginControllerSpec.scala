163e7765c7663070896c949965e553560f5b1ff5
mstoyanovca <mstoyanovca@gmail.com>
2020-02-03 22:11:50 -0500

    added LoginController tests

--- test/controller/LoginControllerSpec.scala
+++ test/controller/LoginControllerSpec.scala
@@ -1,6 +1,7 @@
 package controller
 
-import org.scalatestplus.mockito.MockitoSugar
+import authentication.AuthenticationService
+import dao.User
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerTest
 import play.api.Application
@@ -10,29 +11,77 @@ import play.api.libs.ws.WSClient
 import play.api.mvc._
 import play.api.test.Helpers._
 
-class LoginControllerSpec extends PlaySpec with MockitoSugar with Results with GuiceOneServerPerTest {
-  /* implicit lazy val materializer: Materializer = app.materializer
-   implicit lazy val Action: DefaultActionBuilder = app.injector.instanceOf(classOf[DefaultActionBuilder])
-
-   val mockUserDao: UserDao = mock[UserDao]
-   when(mockUserDao.findByEmailAndPassword(User(None, "a@a.com", Some("password")))).thenReturn(Future(Option(User(Some(1L), "a@a.com", Some("password")))))
-   when(mockUserDao.findByEmail("a@a.com")).thenReturn(Future(Option(User(Some(1L), "a@a.com", Some("password")))))
-
-   val mockAuthenticationService: AuthenticationService = mock[AuthenticationService]
-   when(mockAuthenticationService.loginJwt(User(Some(1L), "a@a.com", None))).thenReturn("jwt_token")
-
-   val mockAuthenticationAction: AuthenticationAction = mock[AuthenticationAction]*/
+class LoginControllerSpec extends PlaySpec with Results with GuiceOneServerPerTest {
 
   override def fakeApplication(): Application = {
-    GuiceApplicationBuilder().configure(Map("play.filters.disabled.0" -> "play.filters.hosts.AllowedHostsFilter")).build()
+    GuiceApplicationBuilder()
+      .configure("play.filters.disabled.0" -> "play.filters.hosts.AllowedHostsFilter",
+        "play.filters.disabled.1" -> "play.filters.csrf.CSRFFilter")
+      .build()
+  }
+
+  "LoginController on login" should {
+    "return a user with a token" in {
+      val wsClient = app.injector.instanceOf[WSClient]
+      val url = s"http://localhost:$port/login"
+
+      val response = await(wsClient.url(url).post(Json.parse("""{ "email": "a@a.com", "password": "password" }""")))
+
+      val user: User = Json.parse(response.body).validate[User].fold(
+        errors => {
+          null
+        },
+        user => {
+          user
+        })
+
+      response.status mustBe OK
+      response.contentType mustBe "application/json"
+
+      user.password must be(None)
+      user.token.get.length must be > 0
+    }
   }
 
   "LoginController on forgotPassword" should {
     "return a user with a token" in {
       val wsClient = app.injector.instanceOf[WSClient]
       val url = s"http://localhost:$port/forgotPassword"
+
       val response = await(wsClient.url(url).post(Json.parse("""{ "forgotPasswordEmail": "a@a.com" }""")))
+
+      val user: User = Json.parse(response.body).validate[User].fold(
+        errors => {
+          null
+        },
+        user => {
+          user
+        })
+
       response.status mustBe OK
+      response.contentType mustBe "application/json"
+
+      user.password must be(None)
+      user.token.get.length must be > 0
+    }
+  }
+
+  "LoginController on changePassword" should {
+    "return a user with a token" in {
+      val wsClient = app.injector.instanceOf[WSClient]
+      val service = app.injector.instanceOf[AuthenticationService]
+      val jwtToken = service.passwordResetJwt(User(Some(1L), "a@a.com", Some("password")))
+      val url = s"http://localhost:$port/changePassword"
+
+      val response = await(
+        wsClient
+          .url(url)
+          .withHttpHeaders("Authorization" -> ("Bearer " + jwtToken))
+          .post(Json.parse("""{ "newPassword": "password" }"""))
+      )
+
+      response.status mustBe OK
+      response.contentType mustBe "text/plain"
     }
   }
 }
